01d54653d040e17d7fc60338517a2c57dd67d67a
Serhii Smirnov <snoff87@gmail.com>
2023-12-03 20:48:11 +0200

    task correct

--- venv/Day_1_task_2.py
+++ venv/Day_1_task_2.py
@@ -1,27 +1,21 @@
-import re
-
-def modify_string(str):
-    rep = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5" \
-        , "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-
-    rep = dict((re.escape(k), v) for k, v in rep.items())
-    pattern = re.compile("|".join(rep.keys()))
-    return pattern.sub(lambda m: rep[re.escape(m.group(0))], str)
 
 def count_calibration_values(file):
+    digits_map = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5 \
+        , "six": 6, "seven": 7, "eight": 8, "nine": 9}
     Lines = file.readlines()
     sum = 0
     for line in Lines:
-        modified_line = modify_string(line.strip())
-        for sym in modified_line:
-            if sym.isdigit():
-                num1 = str(sym)
-                break
-        for sym in modified_line[::-1]:
-            if sym.isdigit():
-                num2 = str(sym)
-                break
-        sum += int(num1 + num2)
+        digits = []
+        line = line.strip()
+        for idx, letter in enumerate(line):
+            if letter.isdigit():
+                digits.append(int(letter))
+                continue
+            for word, digit in digits_map.items():
+                if (idx + len(word)) <= len(line) and word == line[idx:idx + len(word)]:
+                    digits.append(digit)
+        sum += digits[0] * 10 + digits[-1]
     return sum
+
 file1 = open('Input.txt', 'r')
 print(count_calibration_values(file1))
