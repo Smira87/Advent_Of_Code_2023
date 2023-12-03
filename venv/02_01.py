0735496cea85c50a706ea9494b75d2d9627b3c0d
Serhii Smirnov <snoff87@gmail.com>
2023-12-03 22:35:32 +0200

    02_01 better

--- venv/02_01.py
+++ venv/02_01.py
@@ -1,17 +1,21 @@
 # only 12 red cubes, 13 green cubes, and 14 blue cubes
 
-colors_map = {"r": 12, "g": 13, "b": 14}
+colors_map = {"red": 12, "green": 13, "blue": 14}
 
 file1 = open('02.data.txt', 'r')
 ans = 0
-game_N = 1
+
 for line in file1:
-    line = line.strip().split(":")[1]
-    ans += game_N
-    for idx in range(len(line)):
-        if line[idx].isdigit() and line[idx + 1].isdigit():
-            if int(line[idx] + line[idx + 1]) > colors_map[line[idx + 3]]:
-                ans -= game_N
-                break
-    game_N += 1
+    game, turn = line.strip().split(": ")
+    game = game.split()[1]
+    possible = True
+
+    for move in turn.split("; "):
+        for singe_move in move.split(", "):
+            number, color = singe_move.split()
+            if int(number) > colors_map[color]:
+                possible = False
+    if possible:
+        ans += int(game)
+
 print(ans)
